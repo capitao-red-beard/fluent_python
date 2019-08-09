b218f024b0defed53a1c566102de504dfe46a494
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-08-09 12:01:00 +0200

    pep-8 fixes

--- french_deck.py
+++ french_deck.py
@@ -2,65 +2,65 @@ import collections
 from random import choice
 
 
-# creating a named tuple to represent a card
+# Creating a named tuple to represent a card.
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 
-# creating a class to represent a french deck of cards
+# Creating a class to represent a french deck of cards.
 class FrenchDeck:
-    # defining the different values associated with a card
+    # Defining the different values associated with a card.
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
-    # initialisation of a deck of cards
+    # Initialisation of a deck of cards.
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
     
-    # returns the length of a set of cards
+    # Returns the length of a set of cards.
     def __len__(self):
         return len(self._cards)
 
-    # returns a cards position in the deck
+    # Returns a cards position in the deck.
     def __getitem__(self, position):
         return self._cards[position]
 
-# create a namedtuple of the deck of cards
+# Create a namedtuple of the deck of cards.
 beer_card = Card('7', 'diamonds')
 
-#  print the card we just created
+# Print the card we just created.
 print(beer_card)
 p
-# initialise an object of FrenchDeck
+# Initialise an object of FrenchDeck.
 deck = FrenchDeck()
 
-# return the length of the deck of cards
+# Return the length of the deck of cards.
 print(len(deck))
 
-# print the first card in the deck
+# Print the first card in the deck.
 print(deck[0])
 
-# print a slice of cards from the deck
+# Print a slice of cards from the deck.
 print(deck[12::13])
 
-# print a random card from the deck
+# Print a random card from the deck.
 print(choice(deck))
 
-# print a reversed version of the deck of cards
+# Print a reversed version of the deck of cards.
 for card in reversed(deck):
     print(card)
 
-# check if a card exisits in the deck
+# Check if a card exisits in the deck.
 print(Card('Q', 'hearts') in deck)
 
-# assign values to each suit in a deck of cards so we can sort them
+# Assign values to each suit in a deck of cards so we can sort them.
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
-# returns ranks for each card in a deck
+# Returns ranks for each card in a deck.
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
 
-# returns a sorted deck of cards
+# Returns a sorted deck of cards.
 for card in sorted(deck, key=spades_high):
     print(card)
--- vector.py
+++ vector.py
@@ -1,52 +1,52 @@
 from math import hypot
 
-# create our vector class
+# Create our vector class.
 class Vector:
 
-    # initialise for a vector
+    # Initialise for a vector.
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
     
-    # returns string representation of an object for inspection
-    # without this vector instances just show memory location
-    # implement __repr__ because python defaults to instead of __str__
+    # Returns string representation of an object for inspection.
+    # Without this vector instances just show memory location.
+    # Implement __repr__ because python defaults to instead of __str__.
     def __repr__(self):
         return f'Vector({self.x}, {self.y})'
 
-    # returns the absolute value of our vector object
+    # Returns the absolute value of our vector object.
     def __abs__(self):
         return hypot(self.x, self.y)
 
-    # if __bool__ not created python tries __len__
-    # if __len__ == 0 return false
-    # return false if magnitude of vector is 0
+    # If __bool__ not created python tries __len__.
+    # If __len__ == 0 return false.
+    # Return false if magnitude of vector is 0.
     def __bool__(self):
         return bool(abs(self))
 
-    # implements "+" operator for objects of this class
+    # Implements "+" operator for objects of this class.
     def __add__(self, other):
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
     
-    # implements "*" operator for objects of this class
+    # Implements "*" operator for objects of this class.
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
 
-# instatiate our vector object(s)
+# Instatiate our vector object(s).
 v = Vector(5, 6)
 v2 = Vector(4, 5)
 
-# print a string representation of the vector obejct(s)
+# Print a string representation of the vector obejct(s).
 print(v)
 print(v2)
 
-# perform addition on our vector objects
+# Perform addition on our vector objects.
 print(v + v2)
 
-# perform a bool check on our vector object
+# Perform a bool check on our vector object.
 print(bool(v))
 
-# print the absolute value of our vector
+# Print the absolute value of our vector.
 print(abs(v))
