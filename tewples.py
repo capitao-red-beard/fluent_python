016dc52d467dff4aaf2573883eb98533fe55e48f
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-08-12 15:03:04 +0200

    added mroe code to the file

--- tewples.py
+++ tewples.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple, OrderedDict
 
 
 # Using tuples as records.
@@ -84,3 +85,52 @@ for name, cc, pop, (latitude, longitude) in metro_areas:
     # Limit the output to metropolitan areas in the western hemisphere.
     if longitude <= 0:
         print(fmt.format(name, latitude, longitude))
+
+# Named Tuples.
+
+# Defining a named tuple used to hold information about a city.
+# Two parameters are required to create a named tuple: a class name and 
+# a list of field names, which can be given as an iterable of strings
+# or as a single space delimeted string (as in the example below).
+City = namedtuple('City', 'name country population coordinates')
+# Data must be passed as positional arguments to the constructor 
+# (in contrast the tuple takes a single iterable).
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo)
+# Fields can be accessed ny name or position.
+print(tokyo.population)
+print(tokyo.coordinates)
+print([tokyo[1]])
+
+# Named tuple has a few attributes alongside the inhereted ones from
+# tuple. The most useful are: _fields - (class attribute),
+# _make(iterable) - (class method) and _asdict() - (instance method).
+# _fields is a tuple with the field names of the class.
+print(City._fields)
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+# _make() allow you to instatiate a named tuple from an iterable.
+delhi = City._make(delhi_data)
+# _asdict() returns a collections.OrderedDict built from the named 
+# tuple instance. Used to produce a nice display of city data.
+print(delhi._asdict())
+OrderedDict([('name', 'Delhi NCR'), ('country', 'IN'), ('population', 21.935),
+             ('coordinates', LatLong(lat=28.613889, long=77.208889))])
+for key, value in delhi._asdict().items():
+    print(f'{key} : {value}')
+
+# Slicing: [start:stop:step].
+l = [10, 20, 30, 40, 50, 60]
+# Splits at 2.
+print(l[:2])
+# Splits at 3.
+print(l[:3])
+print(l[3:])
+# Slicing Objects.
+s = 'bicycle'
+# Every third value.
+print(s[::3])
+# Reversed.
+print(s[::-1])
+# Reversed every other value.
+print(s[::-2])
