906e2fdfe7125231b0314ff006a23038d40a13a3
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-08-14 14:19:39 +0200

    added sets code and preparing for text vs bytes

---
+++ part_2/sets.py
@@ -0,0 +1,41 @@
+from dis import dis
+from unicodedata import name
+
+
+# Set theory.
+# Set type and its immutable sibling frozenset.
+# A set is a collection of unique objects.
+l = ['spam', 'spam', 'eggs', 'eggs']
+print(set(l))
+print(list(set(l)))
+
+# Set elements must be hashable. The set type itself is not hashable, 
+# but frozenset is.
+'''
+found = len(needles & haystack) # Requires both to be sets.
+would be written as:
+found = 0
+for n in needles:
+    if n in haystack:
+        found += 1
+Solution:
+found = len(set(needles) & set(haystack))
+Another way:
+found = len(set(needles).intersection(haystack))
+'''
+
+# Set literals.
+s = {1}
+print(type(2))
+print(s)
+print(s.pop())
+print(s)
+dis('{1}')
+dis('set([1])')
+
+# Set comprehension.
+# Setcomps.
+# Build a set of characters with codes from 32 to 256 that have the 
+# word 'SIGN' in the name.
+c = {chr(i) for i in range(32, 256) if 'SIGN' in name(chr(i), '')}
+print(c)
