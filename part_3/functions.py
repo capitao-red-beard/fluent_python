c90fca5cb738c7f7f579200fa912947ea68500e2
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-08-20 08:51:48 +0200

    covered more of the bgook for this chapter and added some code.

--- part_3/functions.py
+++ part_3/functions.py
@@ -1,3 +1,7 @@
+from functools import reduce
+from operator import add
+
+
 # Treating a function like an object.
 def factorial(n):
     '''returns n!'''
@@ -27,3 +31,23 @@ def reverse(word):
 
 print(reverse('testing'))
 print(sorted(fruits, key=reverse))
+
+# Modern replacements for map, filter and reduce.
+# Build a list of factorials from 0! to 5!.
+print(list(map(fact, range(6))))
+# With list comp.
+l = [fact(n) for n in range(6)]
+print(l)
+# List of factorials of odd numbers up to 5!.
+print(list(map(factorial, filter(lambda n: n % 2, range(6)))))
+# With list comp.
+l2 = [factorial(n) for n in range(6) if n % 2]
+print(l2)
+# Sum integers up to 99, we imported add to avoid creating an add 
+# function ourselves.
+print(reduce(add, range(100)))
+# Same using sum, no import or adding needed.
+print(sum(range(100)))
+
+# Anonymous functions.
+# Small, one-off functions.
